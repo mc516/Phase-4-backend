0d1e237edf3be765f5d1ead01ee94b9d85bef91a
Mickey <mickeyc11111@gmail.com>
2022-12-18 10:39:23 -0500

    Displayed comments username matches logged in user rather than being unique to each user

---
+++ client/src/EditComment.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {useState} from 'react'
+
+function EditComment({comment, setIsEdit, updateComment, user}) {
+    const [newComment, setNewComment] = useState(comment)
+
+    function handleChange(e){
+        console.log(e.target.value)
+        setNewComment({
+            ...newComment,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function handleSubmit(e){
+        // e.preventdefault()
+        fetch(`/comments/${comment.id}`, {
+            method:'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                body: newComment.body
+            })
+        })
+        .then(res => res.json())
+        .then(updatedComment => updateComment(updatedComment)) 
+        setIsEdit(false)   
+    }
+    
+    return(
+        <div className="edit-comment">
+            <form onSubmit={handleSubmit}> 
+                <strong><p>{user.name.split(' ')[0] + " " + user.name.split(' ')[1][0]} :</p></strong>
+                <textarea
+                    type="text-box"
+                    name="body"
+                    autoComplete="off"
+                    rows="5" 
+                    cols="40"
+                    value={newComment.body}
+                    onChange={handleChange}
+                />
+                <input type="submit" value="save"/>
+            </form>
+        </div>
+    )
+}
+
+export default EditComment;
